16ac46e47ed2e8af08c8bf8422e1196923fc6db1
anusha-xox <anushajain.bang@gmail.com>
2023-02-08 02:05:54 +0530

    framework functionality added

--- form_data.py
+++ form_data.py
@@ -5,10 +5,17 @@ from flask_bootstrap import Bootstrap
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 import email_validator
+from wtforms.fields.html5 import DateField
+from wtforms.validators import ValidationError
+from main import Guide
 
 REG_CATEGORY = ["GM", "SC", "ST", "Physically Handicapped", "Other"]
 GENDER = ["Male", "Female", "Non-Binary", "Other", "Prefer not to say"]
 DURATION_TYPE = ["Full Time", "Part Time"]
+DESIGNATION = ["Assistant Professor", "Associate Professor", "Professor", "HOD"]
+ASSO_RC = ["CSE", "ISE"]
+PROJECT_STATUS = ["Completed", "Ongoing"]
+PROJECT_TYPE = ["Research", "Consultancy"]
 
 
 class LoginForm(FlaskForm):
@@ -25,7 +32,6 @@ class RegisterForm(FlaskForm):
 
 
 class CandidateForm(FlaskForm):
-    __bind_key__ = 'candidate'
     vtu_no = StringField('VTU Registration Number', validators=[DataRequired()])
     c_fname = StringField('First Name', validators=[DataRequired()])
     c_mname = StringField('Middle Name')
@@ -37,6 +43,40 @@ class CandidateForm(FlaskForm):
     reg_year = StringField('Year of Registration', validators=[DataRequired()])
     thesis_title = StringField('Title of Thesis', validators=[DataRequired()])
     duration_type = SelectField('Duration Type', choices=DURATION_TYPE, validators=[DataRequired()])
-    c_email = StringField('Email', validators=[DataRequired(), Email()])
+    c_email = StringField('Email', validators=[DataRequired(), Email()], render_kw={'readonly': True})
     c_phone = StringField('Phone No.', validators=[DataRequired()])
+    c_guide = SelectField("Choose Guide")
+
+    def __init__(self, *args, **kwargs):
+        super(CandidateForm, self).__init__(*args, **kwargs)
+        self.c_guide.choices = [c.g_email for c in Guide.query.all()]
+
     submit = SubmitField(label='Submit')
+
+
+class GuideForm(FlaskForm):
+    g_fname = StringField('First Name', validators=[DataRequired()])
+    g_mname = StringField('Middle Name')
+    g_lname = StringField('Last Name')
+    g_designation = SelectField('Designation', choices=DESIGNATION, validators=[DataRequired()])
+    g_arcenter = SelectField('Associated Research Center', choices=ASSO_RC, validators=[DataRequired()])
+    g_email = StringField('Email ID', validators=[DataRequired(), Email()])
+    g_password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Submit')
+
+
+class ProjectForm(FlaskForm):
+    grant = StringField('Grand Received', validators=[DataRequired()])
+    date_of_issue = DateField('Date of Issue', format='%Y-%m-%d')
+    date_of_completion = DateField('Date Of Completion', format='%Y-%m-%d')
+    project_type = SelectField("Project Type", choices=PROJECT_TYPE, validators=[DataRequired()])
+    funded_by = StringField("Funded By", validators=[DataRequired()])
+    status = SelectField('Project Status', choices=PROJECT_STATUS, validators=[DataRequired()])
+    title = StringField("Title Given", validators=[DataRequired()])
+    submit = SubmitField(label='Submit')
+
+    def validate_date_of_completion(form, field):
+        if field.data is None:
+            pass
+        elif field.data < form.date_of_issue.data:
+            raise ValidationError("End date must not be earlier than start date.")
--- main.py
+++ main.py
@@ -10,17 +10,17 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 import email_validator
 from flask_bootstrap import Bootstrap
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from form_data import LoginForm, RegisterForm, CandidateForm
+from form_data import *
 
 app = Flask(__name__)
 
-# ADMIN ADDS PROFESSORS, INDUSTRIES, ETC.
+# ADMIN ADDS PROFESSORS
 # one to many bw guide and candidate
 
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_BINDS'] = {'candidate': 'sqlite:///candidate.db',
-                                  'admin': 'sqlite:///admin.db'}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///iseResearch.db'
+# app.config['SQLALCHEMY_BINDS'] = {'candidate': 'sqlite:///candidate.db',
+#                                   'admin': 'sqlite:///admin.db'}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -28,11 +28,6 @@ bootstrap = Bootstrap(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 
 class User(UserMixin, db.Model):
@@ -45,14 +40,12 @@ class User(UserMixin, db.Model):
 
 
 class Candidate(db.Model):
-    __bind_key__ = 'candidate'
     vtu_no = db.Column(db.Integer, primary_key=True)
     c_fname = db.Column(db.String(1000), nullable=False)
     c_mname = db.Column(db.String(1000))
     c_lname = db.Column(db.String(1000))
     c_gender = db.Column(db.String(1000))
     reg_category = db.Column(db.String(1000))
-    # reg_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     reg_day = db.Column(db.Integer, nullable=False)
     reg_month = db.Column(db.Integer, nullable=False)
     reg_year = db.Column(db.Integer, nullable=False)
@@ -60,6 +53,33 @@ class Candidate(db.Model):
     duration_type = db.Column(db.String(1000), nullable=False)
     c_email = db.Column(db.String(100), unique=True, nullable=False)
     c_phone = db.Column(db.Integer, unique=True, nullable=False)
+    c_guide = db.Column(db.String(1000))
+
+
+class Guide(db.Model):
+    g_id = db.Column(db.Integer, primary_key=True)
+    g_fname = db.Column(db.String(1000), nullable=False)
+    g_mname = db.Column(db.String(1000))
+    g_lname = db.Column(db.String(1000))
+    g_designation = db.Column(db.String(1000))
+    g_arcenter = db.Column(db.String(1000))
+    g_email = db.Column(db.String(1000))
+
+
+class ProjectDetails(db.Model):
+    p_id = db.Column(db.Integer, primary_key=True)
+    grant = db.Column(db.String(1000), nullable=False)
+    date_of_issue = db.Column(db.String(1000), nullable=False)
+    date_of_completion = db.Column(db.String(1000), nullable=False)
+    project_type = db.Column(db.String(1000), nullable=False)
+    funded_by = db.Column(db.String(1000), nullable=False)
+    status = db.Column(db.String(1000), nullable=False)
+    title = db.Column(db.String(1000), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
     # content = db.Column(db.Text, nullable=False)
     # user_id = db.Column(db.Integer, db.ForeignKey("table_name.column_name"), nullable=False)
@@ -88,7 +108,8 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         display_name = form.username.data
-        return redirect(url_for('candidate_details', display_name=display_name))
+        display_email = form.email.data
+        return redirect(url_for('candidate_details', display_name=display_name, c_email=display_email))
     return render_template("enter.html", form=form, title_given="Register")
 
 
@@ -111,14 +132,23 @@ def login():
             user = User.query.filter_by(email=email).first()
             if check_password_hash(user.password, password):
                 login_user(user)
-                return redirect(url_for('candidate_details', display_name=user.username))
+                for c in Candidate.query.all():
+                    if user.email == c.c_email:
+                        return redirect(url_for("candidate_dashboard", vtu_no=c.vtu_no))
+                for g in Guide.query.all():
+                    if user.email == g.g_email:
+                        return redirect(url_for("guide_dashboard", g_id=g.g_id))
+                return redirect(url_for('candidate_details', display_name=user.username, c_email=user.email))
     return render_template('enter.html', form=form, title_given="Login")
 
 
 @app.route('/candidate-details', methods=['GET', 'POST'])
 def candidate_details():
+    display_email = request.args.get("c_email")
     display_name = request.args.get("display_name")
-    form = CandidateForm()
+    form = CandidateForm(
+        c_email=display_email
+    )
     if form.validate_on_submit():
         new_candidate = Candidate(
             vtu_no=form.vtu_no.data,
@@ -133,7 +163,8 @@ def candidate_details():
             thesis_title=form.thesis_title.data,
             duration_type=form.duration_type.data,
             c_email=form.c_email.data,
-            c_phone=form.c_phone.data
+            c_phone=form.c_phone.data,
+            c_guide=form.c_guide.data
         )
         db.session.add(new_candidate)
         db.session.commit()
@@ -141,9 +172,151 @@ def candidate_details():
     return render_template("add_details.html", form=form, display_name=display_name)
 
 
+@app.route("/candidate-dashboard", methods=["GET", "POST"])
+def candidate_dashboard():
+    vtu_no = int(request.args.get("vtu_no"))
+    current_candidate = Candidate.query.get(vtu_no)
+    CANDIDATE_OPTIONS = [
+        "View Details",
+        "Edit Profile",
+        "View Current Projects",
+        "View Messages from Guide",
+        "Upload Documents to Guide"
+    ]
+    CANDIDATE_LINKS = []
+    return render_template(
+        "grid.html",
+        title=f"{current_candidate.c_fname} {current_candidate.c_mname} {current_candidate.c_lname}",
+        grid_options=CANDIDATE_OPTIONS,
+        grid_links=CANDIDATE_LINKS,
+        grid_no=len(CANDIDATE_OPTIONS)
+    )
+
+
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
-    return render_template("admin-home.html")
+    ADMIN_OPTIONS = [
+        "Add Project",
+        "Generate Guide Credentials",
+        "Edit Student Details",
+        "Edit Project",
+        "Edit Guide Details",
+        "View Project Details",
+        "Assign Student to Project",
+        "Assign Professor to Project",
+        "Assign Student to Guide"
+        "Share Circular To All",
+        "Alumni Report",
+        "View All Candidates Registered",
+        "View All Users",
+        "View All Faculties"
+    ]
+    ADMIN_LINKS = [
+        url_for("add_project"),
+        url_for("add_guide"),
+        "",
+        "",
+        "",
+        url_for("view_project_details"),
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ]
+    return render_template(
+        "grid.html",
+        title="Admin",
+        grid_options=ADMIN_OPTIONS,
+        grid_links=ADMIN_LINKS,
+        grid_no=len(ADMIN_OPTIONS)
+    )
+
+
+@app.route("/guide", methods=["GET", "POST"])
+def guide_dashboard():
+    g_id = int(request.args.get("g_id"))
+    current_guide = Guide.query.get(g_id)
+    GUIDE_OPTIONS = [
+        "View Details",
+        "Edit Profile",
+        "View Candidates Under Me",
+        "View My Projects",
+        "Send Message to Candidates"
+    ]
+    GUIDE_LINKS = [
+        "",
+        "",
+        url_for("view_candidates_under", g_email=current_guide.g_email)
+    ]
+    return render_template(
+        "grid.html",
+        title=f"{current_guide.g_fname} {current_guide.g_mname} {current_guide.g_lname}",
+        grid_options=GUIDE_OPTIONS,
+        grid_links=GUIDE_LINKS,
+        grid_no=len(GUIDE_OPTIONS)
+    )
+
+
+@app.route("/view-candidates-under")
+def view_candidates_under():
+    g_email = request.args.get("g_email")
+    current_guide = Guide.query.filter_by(g_email=g_email).first()
+    candidates = Candidate.query.filter_by(c_guide=current_guide.g_email).all()
+    return render_template("view_candidates_under.html", candidates=candidates,
+                           table_heading=f"Candidates Under {current_guide.g_fname}")
+
+
+@app.route("/admin/generate-guide-credentials", methods=["GET", "POST"])
+def add_guide():
+    form = GuideForm()
+    if form.validate_on_submit():
+        new_guide = Guide(
+            g_fname=form.g_fname.data,
+            g_mname=form.g_mname.data,
+            g_lname=form.g_lname.data,
+            g_designation=form.g_designation.data,
+            g_arcenter=form.g_arcenter.data,
+            g_email=form.g_email.data
+        )
+        new_user = User(
+            username="Admin_Generated",
+            email=form.g_email.data,
+            password=generate_password_hash(form.g_password.data, method='pbkdf2:sha256', salt_length=8)
+        )
+        db.session.add(new_guide)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('admin'))
+    return render_template("add_details.html", form=form, display_name="Admin. Generate New Guide below.")
+
+
+@app.route("/admin/add-project", methods=["GET", "POST"])
+def add_project():
+    form = ProjectForm()
+    if form.validate_on_submit():
+        new_project = ProjectDetails(
+            grant=form.grant.data,
+            date_of_issue=form.date_of_issue.data,
+            date_of_completion=form.date_of_completion.data,
+            project_type=form.project_type.data,
+            funded_by=form.funded_by.data,
+            status=form.status.data,
+            title=form.title.data,
+        )
+        db.session.add(new_project)
+        db.session.commit()
+        return redirect(url_for('admin'))
+    return render_template("add_details.html", form=form, display_name="Admin. Enter Project Details below.")
+
+
+@app.route("/view-project-details", methods=["GET", "POST"])
+def view_project_details():
+    all_projects = ProjectDetails.query.order_by("date_of_issue").all()
+    return render_template("view_project_details.html", all_projects=all_projects, table_heading="All Projects")
 
 
 @app.route('/about')
